55de6ff09e0347e0177c1393ed869075f33f4188
Devyani Singh <singhdevyani1020@gmail.com>
2024-01-07 12:01:31 +0530

    blocks

--- src/components/Common/block.js
+++ src/components/Common/block.js
@@ -3,6 +3,7 @@ import Editable from './editable';
 import Dropdown from './dropdown';
 import { useDrag } from 'react-dnd'
 import { ItemTypes } from '../DnD/DragTypes';
+import { FaArrowRotateLeft, FaArrowRotateRight } from "react-icons/fa6";
 
 export const Block = (props) => {
 
@@ -13,7 +14,7 @@ export const Block = (props) => {
     }),
   }))
   const [selectedOption, setSelectedOption] = React.useState("mouse");
-  const [text, setText] =React.useState('10');
+  const [text, setText] = React.useState('10');
 
 
   const MainBlock = () => {
@@ -24,6 +25,10 @@ export const Block = (props) => {
         return StepsForwardBlock()
       case "randomDropdown1":
         return RandomBlock()
+      case "rotateLeftBlock":
+        return RotateLeftBlock();
+      case "rotateRightBlock":
+        return RotateRightBlock();
       default:
         return <></>
     }
@@ -41,8 +46,8 @@ export const Block = (props) => {
           style={{
             opacity: isDragging ? 0.5 : 1,
             cursor: 'move',
-          }} class="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer block" onClick={(e) => { props.onclick(e, selectedOption) }}>
-          {"Move "}<Dropdown id="PositionDropdown" selectedOption={selectedOption} onchange={(e) => { handleSelectChange(e) }} />{" stepssteps"}
+          }} class="flex flex-row flex-wrap bg-blue-500 text-white cursor-pointer blockPosition" onClick={(e) => { props.onclick(e, selectedOption) }}>
+          {"go to  "}<Dropdown id="PositionDropdown" selectedOption={selectedOption} onchange={(e) => { handleSelectChange(e) }} />
         </div>
       </>
     )
@@ -56,22 +61,78 @@ export const Block = (props) => {
     };
     return (
       <>
-        <div
-        id="StepsForwardBlock"
-          ref={drag}
+
+        <div ref={drag}
+          id="StepsForwardBlock"
           style={{
             opacity: isDragging ? 0.5 : 1,
             cursor: 'move',
-          }}
-          className="flex flex-column flex-wrap bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer" onClick={(e) => { props.onclick(e, setText) }}>
+          }} class="flex flex-row flex-wrap bg-blue-500 text-white cursor-pointer blockPosition"
+          onClick={(e) => { props.onclick(e, text) }}>
           {"Move"}
-          <Editable  id="StepsForwardEdit" text={text} onchange={(e) => { handleInputChange(e) }} />
+          <Editable id="StepsForwardEdit" text={text} onchange={(e) => { handleInputChange(e) }} />
           {"steps"}
         </div>
       </>
 
     )
   }
+
+  const RotateLeftBlock = () => {
+
+    const handleRotateChange = (e) => {
+      setText(e.target.value);
+    };
+    return (
+      <>
+
+        <div ref={drag}
+          id="RotateLeftBlock"
+          style={{
+            opacity: isDragging ? 0.5 : 1,
+            cursor: 'move',
+          }} class="flex flex-row flex-wrap bg-blue-500 text-white cursor-pointer blockPosition"
+          onClick={(e) => { props.onclick(e, text) }}>
+          {"turn"}
+          <FaArrowRotateLeft style={{
+            alignSelf: "center",
+            margin: "0.2rem"
+          }} />
+          <Editable id="StepsForwardEdit" text={text} onchange={(e) => { handleRotateChange(e) }} />
+          {"degrees"}
+        </div>
+      </>
+
+    )
+  }
+
+  const RotateRightBlock = () => {
+    const handleRotateChange = (e) => {
+      setText(e.target.value);
+    };
+    return (
+      <>
+
+        <div ref={drag}
+          id="RotateRightBlock"
+          style={{
+            opacity: isDragging ? 0.5 : 1,
+            cursor: 'move',
+          }} class="flex flex-row flex-wrap bg-blue-500 text-white cursor-pointer blockPosition"
+          onClick={(e) => { props.onclick(e, text) }}>
+          {"turn"}
+          <FaArrowRotateRight style={{
+            alignSelf: "center",
+            margin: "0.2rem"
+          }} />
+          <Editable id="StepsForwardEdit" text={text} onchange={(e) => { handleRotateChange(e) }} />
+          {"degrees"}
+        </div>
+      </>
+
+    )
+  }
+
   const RandomBlock = () => {
     return (
       <>
--- src/components/Motions/Motions.js
+++ src/components/Motions/Motions.js
@@ -10,7 +10,7 @@ export const Motions = () => {
     cat.style.position = 'absolute';
     let div = document.getElementById('PreviewArea');
     let rect = div.getBoundingClientRect();
-   
+
     if (selectedOption === "mouse") {
       div.style.position = '';
 
@@ -19,37 +19,82 @@ export const Motions = () => {
       cat.style.left = left + 'px';
       cat.style.top = top + 'px';
     }
-    else{
+    else {
       div.style.position = 'relative';
 
-     let divWidth = rect.right - rect.left;
-     let divHeight = rect.bottom - rect.top;
-    
-     let left = Math.floor(Math.random() * divWidth);
-     let top = Math.floor(Math.random() * divHeight);
+      let divWidth = rect.right - rect.left;
+      let divHeight = rect.bottom - rect.top;
 
-     cat.style.left = left + 'px';
+      let left = Math.floor(Math.random() * divWidth);
+      let top = Math.floor(Math.random() * divHeight);
+
+      cat.style.left = left + 'px';
       cat.style.top = top + 'px';
     }
 
   }
 
-  const StepsForwardBlockEvent=(e,text)=>{
-  console.log(e);
-  console.log(text);
+  const StepsForwardBlockEvent = (e, text) => {
+
+    let cat = document.getElementById("Catty");
+    cat.style.display = 'block';
+    cat.style.position = 'absolute';
+    let div = document.getElementById('PreviewArea');
+    let rect = div.getBoundingClientRect();
+
+    let currentLeft = parseInt(window.getComputedStyle(cat).left);
+    let currentRight = parseInt(window.getComputedStyle(cat).right);
+
+    let newLeft = currentLeft + parseInt(text);
+    let newRight_ = currentRight - parseInt(text);
+
+    let catu = cat.getBoundingClientRect();
+    let newLeft_ = Math.max(0, Math.min(rect.width - catu.width, newLeft));
+
+    // Apply new left 
+    if (newRight_ > 0)
+      cat.style.left = `${newLeft}px`;
+
 
   }
+
+  const RotateBlockBlockEvent = (e, text) => {
+
+    let cat = document.getElementById("Catty");
+    cat.style.display = 'block';
+    cat.style.position = 'absolute';
+    let div = document.getElementById('PreviewArea');
+    let rect = div.getBoundingClientRect();
+    let matrix = window.getComputedStyle(cat).transform;
+    let values = matrix == 'none' ? 0 : matrix.match(/matrix\(([^)]+)\)/i)[1].split(',').map(parseFloat);
+    let rotation = values == 0 ? 0 : Math.atan2(values[1], values[0]) * (180 / Math.PI);
+    if (e.target.id === 'RotateRightBlock') {
+      rotation += parseInt(text);
+    } else {
+      rotation -= parseInt(text);
+    }
+    rotation = ((rotation % 360) + 360) % 360;
+    cat.style.transform = `rotate(${rotation}deg)`;
+  }
+
 
   const blockEvents = (e, selectedOption) => {
     switch (e.target.id) {
       case "PositionBlock":
         RandomPositionBlockEvent(e, selectedOption);
         break;
-        case "StepsForwardBlock":
-          StepsForwardBlockEvent(e, text);
+      case "StepsForwardBlock":
+        StepsForwardBlockEvent(e, selectedOption);
         break;
+      case "RotateRightBlock":
+        RotateBlockBlockEvent(e, selectedOption);
+        break;
+        case "RotateLeftBlock":
+          RotateBlockBlockEvent(e, selectedOption);
+          break;
+  
 
-        
+
 
       default:
         break;
@@ -60,8 +105,13 @@ export const Motions = () => {
   return (
     <>
       <Block type="postionBlock" onclick={(e, se) => { blockEvents(e, se) }} />
-      
+
       <Block type="stepsForward" onclick={(e, se) => { blockEvents(e, se) }} />
+
+      <Block type="rotateLeftBlock" onclick={(e, se) => { blockEvents(e, se) }} />
+
+      <Block type="rotateRightBlock" onclick={(e, se) => { blockEvents(e, se) }} />
+
 
     </>
   )
